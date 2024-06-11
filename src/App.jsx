35c0ab2c785351c1c6e708336717fff6258f4db4
ansam007 <anshul.sharma5271@gmail.com>
2024-06-12 00:08:56 +0530

    Initial commit with Vite project setup

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import MainHeader from "./components/MainHeader";
+import PostList from "./components/PostList";
+
+function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
+  function showModalHandler() {
+    setModalIsVisible(true);
+  }
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+
+  return (
+    <>
+      <MainHeader onCreatePost={showModalHandler} />
+      <main>
+        <PostList
+          isPosting={modalIsVisible}
+          onStopPosting={hideModalHandler}
+        />
+      </main>
+    </>
+  );
+}
+
+export default App;
